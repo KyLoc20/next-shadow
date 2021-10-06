b62ecabc7c1f03bde2c1bf383b9b8f5801706309
TaikongRen <liuzeduo1234@gmail.com>
2021-10-06 18:47:38 +0800

    feat: [LearnNextCard]

--- components/home/LearnNextCard.tsx
+++ components/home/LearnNextCard.tsx
@@ -1,16 +1,76 @@
 import * as React from "react";
 import styled from "@emotion/styled";
-const Component = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-width: 100%;
-height: 950px;
-background:rgba(0,0,213,0.1);
-`;
+import { useCustomText, HTMLTag, CustomTextType } from "@/hooks/Text";
+import { useCustomButton, CustomButtonType } from "@/hooks/Button";
+import { Link } from "../generic/Link";
+
 type LearnNextProps = {
   children?: React.ReactNode;
 };
 export default function LearnNextCard(props: LearnNextProps) {
-  return <Component>LearnNextCard</Component>;
+  const [TitleText] = useCustomText(
+    HTMLTag.div,
+    CustomTextType.Title_main32,
+    "center"
+  );
+  const [SubTitleText] = useCustomText(
+    HTMLTag.div,
+    CustomTextType.Title_main16,
+    "center"
+  );
+  const [StartButton] = useCustomButton(CustomButtonType.Content_h45_primary);
+  return (
+    <Component>
+      <Content>
+        <Title>
+          <TitleText>Learn Next.js</TitleText>
+        </Title>
+        <SubTitle>
+          <SubTitleText>
+            Learn Next.js step-by-step and earn points ✨.
+          </SubTitleText>
+        </SubTitle>
+        <Figure>
+          <Link href="https://nextjs.org/learn/basics/create-nextjs-app">
+            <FigureImage></FigureImage>
+          </Link>
+        </Figure>
+        <StartButton>Get Started</StartButton>
+      </Content>
+    </Component>
+  );
 }
+const Component = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 1024px;
+`;
+
+const Content = styled.div`
+  display: flex;
+  padding: 100px 16px;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const Title = styled.div`
+  margin-bottom: 16px;
+`;
+const SubTitle = styled.div`
+  margin-bottom: 60px;
+`;
+const Figure = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  margin-bottom: 60px;
+  //paper
+  box-shadow: 0 8px 30px rgba(0, 0, 0, 0.12);
+`;
+const FigureImage = styled.div`
+  width: 926px;
+  height: 495px;
+  background: url(https://nextjs.org/_next/image?url=%2Fstatic%2Fimages%2Flearn.png&w=1920&q=75);
+  background-size: contain;
+`;
